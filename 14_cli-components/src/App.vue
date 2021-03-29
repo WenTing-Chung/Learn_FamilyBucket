1a648c4a250b3873eef25541e6578f3e24a422e9
WonTing <48178778+WonTing@users.noreply.github.com>
2021-03-29 14:35:02 +0800

    Vue2 components 元件

---
+++ 14_cli-components/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <TitleComponent :title="title" />
+    <!-- 1.使用 function 設定來執行修改 -->
+    <!-- <InputComponent :text="title" @textChange="changeHandler" /> -->
+
+    <!-- 2.使用 sync 同步資料 -->
+    <InputComponent :text.sync="title" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TitleComponent from './components/TitleComponent.vue'
+import InputComponent from './components/InputComponent/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TitleComponent,
+    InputComponent,
+  },
+  data() {
+    return {
+      title: 'HelloWorld',
+    }
+  },
+  //-- 1.使用 function 設定來執行修改
+  // methods: {
+  //   changeHandler(val) {
+  //     this.title = val
+  //   },
+  // },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 14_cli-components/src/components/InputComponent/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <input v-model="inputText" type="text" />
+</template>
+
+<script>
+export default {
+  name: 'InputComponent',
+  props: {
+    text: {
+      type: String,
+      required: true,
+      // default: '',
+    },
+  },
+  computed: {
+    inputText: {
+      get() {
+        return this.text
+      },
+      set(val) {
+        // console.log(val)
+        //-- 1.使用 function 設定來執行修改
+        // this.$emit('textChange', val)
+
+        //-- 2.使用 sync 同步資料
+        this.$emit('update:text', val)
+      },
+    },
+  },
+}
+</script>
+
+<style src="./style.css"></style>
